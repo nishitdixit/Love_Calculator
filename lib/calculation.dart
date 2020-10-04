253fb8826a126127e0830aa8fa5f20f33e714925
Nishit <nishitdxt@gmail.com>
2020-10-04 14:57:52 +0530

    complete working calculator

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,20 +1,11 @@
-
-
-
-void main() {
-  String firstName ;
-
-  String secondName ;
-
-  // Calculation(this.firstName,this.secondName);
-
+int calculation({String firstName, String secondName}) {
   int finalScore;
   List<String> firstList = firstName.toLowerCase().split('');
   List<String> secondList = secondName.toLowerCase().split('');
 
 //making complete word "firstName + loves + second name"
   List<String> magicWord = firstList + "loves".split('') + secondList;
-
+  print(magicWord);
 //Counting occurence of letters and storing them into map
   var nameMap = new Map();
   magicWord.forEach((f) {
@@ -36,19 +27,23 @@ void main() {
 //   print(magicCode);
   finalScore = magicCode[0] * 10 + magicCode[1];
   print(finalScore);
+  return finalScore;
 }
 
 //calculation in loop
 List _calculate(List magicCode) {
-  int i = 0;
+  int countSum;
   List score = new List();
   do {
-    score.add(magicCode.first + magicCode.last);
+    countSum=magicCode.first + magicCode.last;
+    if(countSum>9) countSum-=9;
+    score.add(countSum);
     magicCode.removeLast();
     magicCode.removeAt(0);
-    i++;
-//     print(i);
   } while (magicCode.length >= 2);
-
+  print(magicCode);
+  if (magicCode.isNotEmpty) {
+    score.add(magicCode.first);
+  }
   return score;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'calculation.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,11 +21,14 @@ class MyCalculator extends StatefulWidget {
 }
 
 class _MyCalculatorState extends State<MyCalculator> {
+  String firstName;
+  String secondName;
   int score;
   void loveScore() {
-    setState(() {
-      score = Random().nextInt(40) + 40;
-    });
+    // score = Random().nextInt(40) + 40;
+    score = calculation(firstName: firstName, secondName: secondName);
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => ScorePage(score: score)));
   }
 
   @override
@@ -36,30 +38,100 @@ class _MyCalculatorState extends State<MyCalculator> {
         title: Text('LOVE CALCULATOR'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-                border: InputBorder.none, hintText: 'Enter your name'),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Hero(
+                    tag: 'lovegif',
+                    child: Image(image: AssetImage('assets/images/logo.gif')),
+                  ),
+                )),
+            TextField(
+              onChanged: (value) {
+                firstName = value;
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+              ),
+            ),
+            TextField(
+              onChanged: (value) {
+                secondName = value;
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your parterner\'s name',
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                loveScore();
+              },
+              child: Text('Calculate'),
+              color: Colors.blue,
+            ),
+          ]),
+    );
+  }
+}
+
+class ScorePage extends StatelessWidget {
+  const ScorePage({
+    Key key,
+    @required this.score,
+  }) : super(key: key);
+
+  final int score;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center,children: [
+            Flexible(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Hero(
+            tag: 'lovegif',
+            child: Image(image: AssetImage('assets/images/logo.gif')),
           ),
-          TextField(
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Enter your partner\'s name'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              loveScore();
-            },
-            child: Text('Calculate'),
-            color: Colors.blue,
-          ),
-          Text(
-            'Your Score is : $score',
-          )
-        ],
+        )),
+            Text(
+        'Your Score is : $score',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
       ),
+          ]),
     );
   }
 }
